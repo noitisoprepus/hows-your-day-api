53ffcc75a7e8c735119b8aa5d1659d8ca0b4719c
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-05-28 22:05:23 +0800

    Implement IDayRepository

--- HowsYourDayApi/Domain/Interfaces/IDayRepository.cs
+++ HowsYourDayApi/Domain/Interfaces/IDayRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace HowsYourDayApi.Domain.Interfaces
+﻿using HowsYourDayApi.Domain.Entities;
+
+namespace HowsYourDayApi.Domain.Interfaces
 {
     public interface IDayRepository
     {
+        Task<IEnumerable<Day>> GetAllAsync();
+        Task<Day> GetByIdAsync(Guid id);
+        Task<IEnumerable<Day>> SearchAsync(
+            Guid? userId = null, 
+            DateTime? searchDateFromUtc = null, DateTime? searchDateToUtc = null);
+        Task InsertAsync(Day day);
+        Task UpdateAsync(Day day);
+        Task DeleteAsync(Day day);
     }
 }
--- HowsYourDayApi/Infrastructure/Repositories/DayRepository.cs
+++ HowsYourDayApi/Infrastructure/Repositories/DayRepository.cs
@@ -1,4 +1,5 @@
-﻿using HowsYourDayApi.Domain.Interfaces;
+﻿using HowsYourDayApi.Domain.Entities;
+using HowsYourDayApi.Domain.Interfaces;
 using HowsYourDayApi.Infrastructure.Persistence;
 
 namespace HowsYourDayApi.Infrastructure.Repositories
@@ -10,6 +11,42 @@ namespace HowsYourDayApi.Infrastructure.Repositories
         public DayRepository(HowsYourDayAppDbContext context)
         {
             _context = context;
+        }
+
+        public Task<IEnumerable<Day>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Day> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Day>> SearchAsync(Guid? userId = null, DateTime? searchDateFromUtc = null, DateTime? searchDateToUtc = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task InsertAsync(Day day)
+        {
+            _context.Days.Add(day);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Day day)
+        {
+            _context.Days.Update(day);
+
+            await _context.SaveChangesAsync();
+        }
+        
+        public async Task DeleteAsync(Day day)
+        {
+            _context.Days.Remove(day);
+
+            await _context.SaveChangesAsync();
         }
     }
 }
