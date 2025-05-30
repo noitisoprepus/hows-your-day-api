5a5d4db4a04f2613fe71a91f81ff7ce0bd2a1289
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-05-30 23:50:34 +0800

    Rename DaySummary.DateUtc to DaySummary.DateStampUtc

--- HowsYourDayApi/Domain/Entities/DaySummary.cs
+++ HowsYourDayApi/Domain/Entities/DaySummary.cs
@@ -3,7 +3,7 @@ namespace HowsYourDayApi.Domain.Entities
     public class DaySummary
     {
         public Guid Id { get; set; }
-        public DateTime DateUtc { get; set; }
+        public DateTime DateStampUtc { get; set; }
         public decimal AverageRating { get; set; }
     }
 }
