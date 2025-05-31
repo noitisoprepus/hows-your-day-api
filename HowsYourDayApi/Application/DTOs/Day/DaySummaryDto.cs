cce80f114559070f273af65e37c0b190508d6822
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-05-31 23:06:22 +0800

    Create DaySummary DTO

---
+++ HowsYourDayApi/Application/DTOs/Day/DaySummaryDto.cs
@@ -0,0 +1,8 @@
+namespace HowsYourDayApi.Application.DTOs.Day
+{
+    public class DaySummaryDto
+    {
+        public DateTime DateStampUtc { get; set; }
+        public decimal AverageRating { get; set; }
+    }   
+}
