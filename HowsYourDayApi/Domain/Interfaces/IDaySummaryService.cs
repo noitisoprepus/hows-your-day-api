247e5c98be862f9b6950de6d29957689ea30f47b
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-06-09 23:20:54 +0800

    Add methods to IDaySummaryService

--- HowsYourDayApi/Domain/Interfaces/IDaySummaryService.cs
+++ HowsYourDayApi/Domain/Interfaces/IDaySummaryService.cs
@@ -1,7 +1,12 @@
+using HowsYourDayApi.Application.DTOs.Day;
+
 namespace HowsYourDayApi.Domain.Interfaces
 {
     public interface IDaySummaryService
     {
-        Task<int> GetAverageRatingAsync();
+        Task<DaySummaryDto?> GetDaySummaryOfDateAsync(DateTime dateUtc);
+        Task<IEnumerable<DaySummaryDto>> GetDaySummariesAsync(DateTime fromUtc, DateTime toUtc);
+        Task InsertDaySummaryAsync(DaySummaryDto daySummary);
+        Task UpdateDaySummaryAsync(DaySummaryDto daySummary);
     }
 }
